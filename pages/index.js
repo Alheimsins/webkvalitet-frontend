075ea7b1280ca7a1b3d7070941be4eaeed65d33b
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-19 10:07:45 +0100

    Calculate totalscore and adds sorting (patch)

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,14 @@ export async function getServerSideProps(context) {
     }
   }
 
+  const calculateTotalScore = categories => categories.reduce((accumulator, current) => accumulator + current.score, 0)
+
+  // Massages the data a bit
+  const repackedData = data
+        .map(item => ({ ...item, categories: item.result.sort((a, b) => a.title.localeCompare(b.title)), total: calculateTotalScore(item.result) }))
+        .sort((a, b) => (a.total < b.total) ? 1 : -1)
+
   return {
-    props: { data },
+    props: { data: repackedData },
   }
 }
