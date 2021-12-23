6d0e2a3419a5996033e9ec5ccb2fc8c44eaabdad
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-23 09:08:51 +0100

    Adds icons (patch)

--- components/card.js
+++ components/card.js
@@ -1,7 +1,14 @@
-export default function Card ({ name }) {
+import { LinkIcon } from '@heroicons/react/outline'
+
+export default function Card ({ name, url }) {
   return (
     <li className="bg-white shadow overflow-hidden px-4 py-4 sm:px-6 sm:rounded-md">
-      <h1>{name}</h1>
+      <div className="flex justify-between">
+        <h1>{name}</h1>
+        <div>
+          <a href={url} target='_blank' title={`Visit the website for ${name}`}><LinkIcon className='h-5 w-5 text-black hover:text-gray-500'/></a>
+        </div>
+      </div>
     </li>
   )
 }
