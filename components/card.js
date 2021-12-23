d86a4dfde5c71ce93b2cc2a513bc9cbd4f9b8976
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-23 09:25:54 +0100

    Adds link for security headers (patch)

--- components/card.js
+++ components/card.js
@@ -1,4 +1,4 @@
-import { LightBulbIcon, LinkIcon } from '@heroicons/react/outline'
+import { LightBulbIcon, LinkIcon, LockClosedIcon } from '@heroicons/react/outline'
 
 export default function Card ({ name, url }) {
   return (
@@ -7,7 +7,8 @@ export default function Card ({ name, url }) {
         <h1>{name}</h1>
         <div className='flex'>
           <a href={url} target='_blank' title={`Visit the website for ${name}`}><LinkIcon className='h-5 w-5 text-black hover:text-gray-500 mr-3'/></a>
-          <a href={`https://lighthouse-dot-webdotdevsite.appspot.com/lh/html?url=${url}`} target='_blank' title={`Visit the Lighthouse report for ${name}`}><LightBulbIcon className='h-5 w-5 text-black hover:text-gray-500'/></a>
+          <a href={`https://lighthouse-dot-webdotdevsite.appspot.com/lh/html?url=${url}`} target='_blank' title={`Visit the Lighthouse report for ${name}`}><LightBulbIcon className='h-5 w-5 text-black hover:text-gray-500 mr-3'/></a>
+          <a href={`https://securityheaders.com?q=${url}&hide=on&followRedirects=on`} target='_blank' title={`Visit the Securityheaders report for ${name}`}><LockClosedIcon className='h-5 w-5 text-black hover:text-gray-500'/></a>
         </div>
       </div>
     </li>
