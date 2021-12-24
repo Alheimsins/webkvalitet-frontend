2c1059eb3ed47035c999a58df69f6cbc571db19b
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-24 09:45:00 +0100

    Adds circular progress bar for scores (patch)

--- components/score.js
+++ components/score.js
@@ -1,8 +1,30 @@
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
+
+function getColor(score) {
+  if (score < 50) {
+    return '#ff0000';
+  } else if (score < 90) {
+    return '#ffa500';
+  } else {
+    return '#008000';
+  }
+}
+
 export default function Score({ title, score }) {
   return (
     <div className="flex flex-col">
-      <div>{title}</div>
-      <div>{score}</div>
+      <CircularProgressbarWithChildren
+        value={score}
+        text={score}
+        className="w-24 h-24"
+        strokeWidth={10}
+        styles={buildStyles({
+          pathColor: getColor(score),
+          textColor: 'black',
+          trailColor: '#d6d6d6',
+        })}
+      />
+      <div className='text-center mt-2'>{title}</div>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import 'react-circular-progressbar/dist/styles.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
