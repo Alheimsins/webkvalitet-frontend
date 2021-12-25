a17b1aca70cc959f74c1a69bee7bc0c3b7961555
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-25 10:37:23 +0100

    Switches to useData component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
 import Card from "../components/card"
-
-const fetcher = (url) => fetch(url).then(r => r.json())
-const calculateTotalScore = categories => categories.reduce((accumulator, current) => accumulator + current.score, 0)
-const repackData = data => data ? data
-        .map(item => ({ ...item, categories: item.result.sort((a, b) => a.title.localeCompare(b.title)), total: calculateTotalScore(item.result) }))
-        .sort((a, b) => (a.total < b.total) ? 1 : -1) : []
+import useData from "../lib/use-data"
 
 export default function Home() {
-  const { data, error } = useSWR("https://webkvalitet.api.alheimsins.net/fylker", fetcher)
+  const { data, isLoading, isError } = useData("https://webkvalitet.api.alheimsins.net/fylker")
+
+  if (isError) {
+    return (
+      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div>Something went wrong ...</div>
+      </div>
+    )
+  }
+  
+  if (isLoading) {
+    return (
+      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div>
+          Loading ...
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
       <ul role="list" className="space-y-12">
-        {repackData(data).map((result, index) => <Card key={result.id} place={index+1} {...result} />)}
+        {data.map((result, index) => <Card key={result.id} place={index+1} {...result} />)}
       </ul>
     </div>
   )
