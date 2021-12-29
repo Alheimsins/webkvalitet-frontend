c6fe4d328f8a3d9cd186c1fa8e60843cbaf77bbf
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-29 09:00:55 +0100

    Adds basic form for checking site (patch)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-
-import useData from "../lib/use-data"
+import { useState } from 'react'
+import webquality from '@alheimsins/webquality'
 
 import Card from "../components/card"
 import SkeletonLoader from "../components/skeleton-loader"
@@ -8,12 +8,33 @@ import SkeletonLoader from "../components/skeleton-loader"
 const Header = () => (
   <Head>
     <title>Webkvalitet</title>
-    <meta name="description" content="Webkvalitet sammenligner kvaliteten på ulike sider innen forskjellige kategorier" />
+    <meta name="description" content="Finn webbkvaliteten på nettstedet ditt" />
   </Head>
 )
 
 export default function Home() {
-  const { data, isLoading, isError } = useData("https://webkvalitet.api.alheimsins.net/fylker")
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setIsLoading(true)
+    setData(null)
+    const result = await webquality(document.getElementById('url').value)
+    setData([{ name : 'webkvalitet', id: 'webkvalitet', result }])
+    setIsLoading(false)
+  }
+
+  const UrlForm = () => {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="https://www.example.com" id='url' name='url'/>
+        </form>
+      </div>
+    )
+  }
 
   if (isError) {
     return (
@@ -30,8 +51,17 @@ export default function Home() {
         <Header />
         <ul role="list" className="space-y-12">
           <SkeletonLoader place={'1'}/>
-          <SkeletonLoader place={'2'}/>
-          <SkeletonLoader place={'3'}/>
+        </ul>
+      </div>
+    )
+  }
+
+  if (data) {
+    return (
+      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <Header />
+        <ul role="list" className="space-y-12">
+          {data.map((result, index) => <Card key={result.id} place={index+1} {...result} />)}
         </ul>
       </div>
     )
@@ -39,10 +69,8 @@ export default function Home() {
 
   return (
     <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-      <Header />
-      <ul role="list" className="space-y-12">
-        {data.map((result, index) => <Card key={result.id} place={index+1} {...result} />)}
-      </ul>
+        <Header />
+        <UrlForm />
     </div>
   )
 }
