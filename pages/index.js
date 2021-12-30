b9ba1c159e748fc817c90ee1380a1ee6de04e99f
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-12-30 09:10:32 +0100

    Adds design to input form

--- pages/index.js
+++ pages/index.js
@@ -28,10 +28,33 @@ export default function Home() {
 
   const UrlForm = () => {
     return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="https://www.example.com" id='url' name='url'/>
-        </form>
+      <div className="bg-white shadow sm:rounded-lg mb-4">
+        <div className="px-4 py-5 sm:p-6">
+          <h1 className="text-lg leading-6 font-medium text-gray-900">Check the quality of a website</h1>
+          <div className="mt-2 max-w-xl text-sm text-gray-500">
+            <p>Enter the url for the site you want to check</p>
+          </div>
+          <form className="mt-5 sm:flex sm:items-center" onSubmit={handleSubmit}>
+            <div className="w-full sm:max-w-xs">
+              <label htmlFor="email" className="sr-only">
+                URL
+              </label>
+              <input
+                type="url"
+                name="url"
+                id="url"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                placeholder="https://example.com"
+              />
+            </div>
+            <button
+              type="submit"
+              className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Check
+            </button>
+          </form>
+        </div>
       </div>
     )
   }
