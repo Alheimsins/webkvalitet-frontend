8bf5fcb11db891ab3527590c28c0ecad845add45
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-01-30 09:23:16 +0100

    Adds more sucurity headers (patch)

--- security-headers-config.js
+++ security-headers-config.js
@@ -21,6 +21,26 @@ const headers = [
     key: "Content-Security-Policy",
     value: stringifiedCSP,
   },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
 ];
 
 module.exports = headers;
